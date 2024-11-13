e3440738b68cf56af38a6ebcd69ef894af433dbc
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-13 14:58:17 +0100

    AppFooter

--- src/components/Appfooter.jsx
+++ src/components/Appfooter.jsx
@@ -2,7 +2,9 @@ export default function AppFooter() {
 
     return (
         <footer>
-
+            <div className="container">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum odio in exercitationem doloribus nostrum accusamus eligendi tempora amet corporis!</p>
+            </div>
         </footer>
     )
 }
