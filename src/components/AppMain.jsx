7e043f70e6138c52e7d3d990c4e7a79802e313d7
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-13 15:26:59 +0100

    component card

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,20 +1,11 @@
-import Button from "./Button/Button";
+import Card from "../components/Card/Card.jsx"
 
 export default function AppMain() {
 
     return (
         <main>
             <div className="container">
-                <div class="card">
-                    <img src="https://via.placeholder.com/600x400" alt="Image" className="card-img" />
-                    <div class="card-body">
-                        <h5 class="card-title">TItolo del post</h5>
-                        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus saepe, aperiam ex asperiores dolorum ipsa accusantium, sed rem sapiente esse veniam reiciendis facilis sint nihil neque! Quas illo corrupti quia!</p>
-                    </div>
-                    <div class="card-footer">
-                        <Button />
-                    </div>
-                </div>
+                <Card />
             </div>
         </main>
     )
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import Button from "../Button/Button";
+export default function Card() {
+    return (
+        <div class="card">
+            <img src="https://via.placeholder.com/600x400" alt="Image" className="card-img" />
+            <div class="card-body">
+                <h5 class="card-title">TItolo del post</h5>
+                <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus saepe, aperiam ex asperiores dolorum ipsa accusantium, sed rem sapiente esse veniam reiciendis facilis sint nihil neque! Quas illo corrupti quia!</p>
+            </div>
+            <div class="card-footer">
+                <Button />
+            </div>
+        </div>
+    )
+}
