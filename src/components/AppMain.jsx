d823f06eaecf1436c60d41388b07d45bb5d8ecbb
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-13 15:18:14 +0100

    fix css

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,7 +4,7 @@ export default function AppMain() {
         <main>
             <div className="container">
                 <div class="card">
-                    <img src="https://via.placeholder.com/300x200" alt="Image" class="card-img-top" />
+                    <img src="https://via.placeholder.com/600x400" alt="Image" className="card-img" />
                     <div class="card-body">
                         <h5 class="card-title">TItolo del post</h5>
                         <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus saepe, aperiam ex asperiores dolorum ipsa accusantium, sed rem sapiente esse veniam reiciendis facilis sint nihil neque! Quas illo corrupti quia!</p>
